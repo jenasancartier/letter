74afcefbf5d53829fceb7d7d757b8dc92868fc16
Aimen Khakwani and Jena SanCartier <jena@>
2016-08-10 11:19:20 -0700

    Add upperCase in jQuery for personName and used css to make the letter uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
 $(document).ready(function(event) {
   $("#blanks form").submit(function(event){
-    var personName = $("input#person").val();
-
-    $(".abby").append(personName);
-
+    var personName = $("input#person").val().toUpperCase();
+    $(".abby").text(personName);
     $("#letter").show();
-
     event.preventDefault();
   });
 });
